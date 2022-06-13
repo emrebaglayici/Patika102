374a5f608f17a6c33996de8f3467043867946905
emre <emrebaglayici1@gmail.com>
2022-06-13 18:11:43 +0300

    Homework 12

--- out/production/java102/Odev3.sql
+++
@@ -1,4 +0,0 @@
-SELECT country FROM country WHERE country LIKE 'A%' AND country LIKE '%a';
-SELECT country FROM country WHERE country LIKE '______' AND country LIKE '%n';
-SELECT title FROM film WHERE title ILIKE 'T%T%T%T%';
-SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
