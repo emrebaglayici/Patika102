1bddf9e6b1e1df2b878fccdc01d2834701827d90
emre <emrebaglayici1@gmail.com>
2022-07-14 13:09:50 +0300

    ContentGUI.class edited.

---
+++ src/PatikaClone/Models/Content.java
@@ -0,0 +1,214 @@
+package PatikaClone.Models;
+
+import PatikaClone.Helper.DbConnector;
+import PatikaClone.Helper.Helper;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Content {
+    private int id;
+    private int course_id;
+    private String title;
+    private String description;
+    private String link;
+
+    private Course course;
+
+    public Content(int id, int course_id, String title, String description, String link) {
+        this.id = id;
+        this.course_id = course_id;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.course = Course.getFetchById(course_id);
+
+    }
+
+    public Content() {
+    }
+
+    public static ArrayList<Content> getList(){
+        ArrayList<Content> contents=new ArrayList<>();
+        String query="SELECT * FROM content";
+        Content obj;
+        try {
+            Statement statement= DbConnector.getInstance().createStatement();
+            ResultSet resultSet=statement.executeQuery(query);
+            while (resultSet.next()){
+                obj=new Content();
+                int id=resultSet.getInt("id");
+                int course_id=resultSet.getInt("course_id");
+                String title=resultSet.getString("title");
+                String description=resultSet.getString("description");
+                String link=resultSet.getString("link");
+                obj=new Content(id,course_id,title,description,link);
+                contents.add(obj);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return contents;
+    }
+
+    public static boolean add(int course_id, String title, String desc, String link) {
+        String query="INSERT INTO content(course_id,title,description,link) VALUES(?,?,?,?)";
+        Content findTitle=Content.getFetch(title);
+        if(findTitle!=null){
+            Helper.showMessage("This content title already used.");
+            return false;
+        }
+        try {
+            PreparedStatement pr=DbConnector.getInstance().prepareStatement(query);
+            pr.setInt(1,course_id);
+            pr.setString(2,title);
+            pr.setString(3,desc);
+            pr.setString(4,link);
+            int response=pr.executeUpdate();
+            if (response==-1){
+                Helper.showMessage("error");
+            }
+            return response!=-1;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return true;
+    }
+
+    private static Content getFetch(String title) {
+        Content obj=null;
+        String query="SELECT * FROM content WHERE title=?";
+        try{
+            PreparedStatement pr=DbConnector.getInstance().prepareStatement(query);
+            pr.setString(1,title);
+            ResultSet resultSet= pr.executeQuery();
+            if(resultSet.next()){
+                obj=new Content(
+                        resultSet.getInt("id"),
+                        resultSet.getInt("course_id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("description"),
+                        resultSet.getString("link")
+                );
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return obj;
+    }
+
+    public static boolean delete(int id) {
+        String query="DELETE FROM content WHERE id=?";
+        try {
+            PreparedStatement preparedStatement=DbConnector.getInstance().prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            return preparedStatement.executeUpdate()!=-1;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+
+    }
+
+    private static ArrayList<Content> getListByCourseId(int course_id) {
+        ArrayList<Content> contents=new ArrayList<>();
+        Content obj;
+        try{
+            Statement statement=DbConnector.getInstance().createStatement();
+            ResultSet resultSet= statement.executeQuery(
+                    "SELECT * FROM content WHERE course_id="+course_id
+            );
+            while (resultSet.next()){
+                int id=resultSet.getInt("id");
+                int courseId=resultSet.getInt("course_id");
+                String title=resultSet.getString("title");
+                String desc=resultSet.getString("description");
+                String link=resultSet.getString("link");
+                obj=new Content(id,courseId,title,desc,link);
+                contents.add(obj);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return contents;
+    }
+
+    public static String searchQuery(String content_desc, String content_title) {
+        String query="SELECT * FROM content WHERE description LIKE '%{{description}}%' AND title LIKE '%{{title}}%'";
+        query=query.replace("{{description}}",content_desc);
+        query=query.replace("{{title}}",content_title);
+        return query;
+    }
+
+    public static ArrayList<Content> searchContentList(String query) {
+        ArrayList<Content> contents=new ArrayList<>();
+        Content obj;
+        try {
+            Statement statement=DbConnector.getInstance().createStatement();
+            ResultSet resultSet= statement.executeQuery(query);
+            while (resultSet.next()){
+                obj=new Content();
+                obj.setId(resultSet.getInt("id"));
+                obj.setCourse_id(resultSet.getInt("course_id"));
+                obj.setTitle(resultSet.getString("title"));
+                obj.setDescription(resultSet.getString("description"));
+                obj.setLink(resultSet.getString("link"));
+                contents.add(obj);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return contents;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int course_id) {
+        this.course_id = course_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
--- src/PatikaClone/View/ContentGUI.java
+++ src/PatikaClone/View/ContentGUI.java
@@ -2,9 +2,12 @@ package PatikaClone.View;
 
 import PatikaClone.Helper.Config;
 import PatikaClone.Helper.Helper;
+import PatikaClone.Models.Content;
 import PatikaClone.Models.Course;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
 
 public class ContentGUI extends JFrame{
     private JPanel wrapper;
@@ -17,19 +20,22 @@ public class ContentGUI extends JFrame{
     private JLabel lbl_link;
     private JPanel pnl_left;
     private JButton btn_content_add;
-    private JTextField fld_course_id;
+    private JTextField fld_content_id;
     private JButton btn_content_delete;
-    private JTextField fld_sh_course_name;
+    private JTextField fld_sh_content_desc;
     private JTextField fld_sh_content_title;
     private JButton btn_content_sh;
     private JTable tbl_content_list;
     private JScrollPane scrl_content_list;
+    private JTextField textField1;
+    private DefaultTableModel mdl_content_list;
+    private Object[] row_content_list;
     private final Course course;
 
     public ContentGUI(Course course){
         this.course=course;
         add(wrapper);
-        setSize(500, 500);
+        setSize(1000, 500);
         setLocation(Helper.screenCenterPoint("x",getSize()),
                 Helper.screenCenterPoint("y",getSize()));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -37,5 +43,103 @@ public class ContentGUI extends JFrame{
         setResizable(false);
         setVisible(true);
         lbl_content_name.setText(this.course.getName()+" content");
+
+        mdl_content_list=new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int row,int column){
+                if (column==0)
+                    return false;
+                return super.isCellEditable(row,column);
+            }
+        };
+        Object[] col_content_list={
+                "Id","Course Id","Title","Description","Youtube Link"
+        };
+        mdl_content_list.setColumnIdentifiers(col_content_list);
+        row_content_list=new Object[col_content_list.length];
+        loadContentModel();
+
+        tbl_content_list.setModel(mdl_content_list);
+        tbl_content_list.getTableHeader().setReorderingAllowed(false);
+
+        tbl_content_list.getSelectionModel().addListSelectionListener(e->{
+            try{
+                String select_course_id=tbl_content_list.getValueAt(tbl_content_list.getSelectedRow(),0).toString();
+                fld_content_id.setText(select_course_id);
+            }catch (Exception exception){
+                System.out.println(exception.getMessage());
+            }
+        });
+
+        btn_content_add.addActionListener(e->{
+            if (Helper.isFieldEmpty(fld_content_title)||Helper.isFieldEmpty(fld_content_desc)
+            ||Helper.isFieldEmpty(fld_content_link)){
+                Helper.showMessage("fill");
+            }else{
+                int course_id=course.getId();
+                String title=fld_content_title.getText();
+                String desc=fld_content_desc.getText();
+                String link=fld_content_link.getText();
+                if (Content.add(course_id,title,desc,link)){
+                    Helper.showMessage("done");
+                    loadContentModel();
+                    fld_content_title.setText(null);
+                    fld_content_desc.setText(null);
+                    fld_content_link.setText(null);
+                }
+            }
+        });
+
+        btn_content_delete.addActionListener(e->{
+            if (Helper.isFieldEmpty(fld_content_id)){
+                Helper.showMessage("fill");
+            }else{
+                if (Helper.confirm("sure")){
+                    int content_id=Integer.parseInt(fld_content_id.getText());
+                    if (Content.delete(content_id)){
+                        Helper.showMessage("done");
+                        loadContentModel();
+                        fld_content_id.setText(null);
+                    }else{
+                        Helper.showMessage("error");
+                    }
+                }
+            }
+        });
+        btn_content_sh.addActionListener(e->{
+            String content_desc= fld_sh_content_desc.getText();
+            String content_title=fld_sh_content_title.getText();
+            String query=Content.searchQuery(content_desc,content_title);
+            loadContentModel(Content.searchContentList(query));
+
+        });
     }
+
+    private void loadContentModel() {
+        DefaultTableModel clearmodel=(DefaultTableModel) tbl_content_list.getModel();
+        clearmodel.setRowCount(0);
+        for (Content c:Content.getList()){
+            int i=0;
+            row_content_list[i++]=c.getId();
+            row_content_list[i++]=c.getCourse_id();
+            row_content_list[i++]=c.getTitle();
+            row_content_list[i++]=c.getDescription();
+            row_content_list[i++]=c.getLink();
+            mdl_content_list.addRow(row_content_list);
+        }
+    }
+    private void loadContentModel(ArrayList<Content> list) {
+        DefaultTableModel clearmodel=(DefaultTableModel) tbl_content_list.getModel();
+        clearmodel.setRowCount(0);
+        for (Content c:list){
+            int i=0;
+            row_content_list[i++]=c.getId();
+            row_content_list[i++]=c.getCourse_id();
+            row_content_list[i++]=c.getTitle();
+            row_content_list[i++]=c.getDescription();
+            row_content_list[i++]=c.getLink();
+            mdl_content_list.addRow(row_content_list);
+        }
+    }
+
 }
